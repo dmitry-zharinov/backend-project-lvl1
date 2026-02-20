b4f4dd97c2dcece6243f95d712a9f2a2a3ba578f
Dmitry Zharinov <zharinovdm@yandex.ru>
2026-02-20 20:32:08 +0300

    Реализовано приветствие пользователя в CLI

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+const runCli = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('Как вас зовут? ');
+  console.log(`Привет, ${name}!`);
+};
+
+export default runCli;
